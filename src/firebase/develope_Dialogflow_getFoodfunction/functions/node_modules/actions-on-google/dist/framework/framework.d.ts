0293e436c01af3075cc5eae1a44047d6d2b83faf
LazyerIJ <spozam01@gmail.com>
2018-06-11 19:37:41 +0900

    ninevis

---
+++ src/firebase/develope_Dialogflow_getFoodfunction/functions/node_modules/actions-on-google/dist/framework/framework.d.ts
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ExpressHandler, Express } from './express';
+import { LambdaHandler, Lambda } from './lambda';
+import { JsonObject } from '../common';
+export interface Frameworks {
+    /** @public */
+    [name: string]: Framework<Function>;
+}
+/** @public */
+export interface Framework<THandler> {
+    /** @public */
+    handle(base: StandardHandler): THandler;
+    /** @public */
+    check(...args: any[]): boolean;
+}
+/** @public */
+export interface OmniHandler extends StandardHandler, ExpressHandler, LambdaHandler {
+    /** @public */
+    (...args: any[]): any;
+}
+export interface BuiltinFrameworks extends Frameworks {
+    /**
+     * Plug and play web framework support for express using body-parser
+     * @public
+     */
+    express: Express;
+    /**
+     * Plug and play web framework support for lambda API gateway
+     * @public
+     */
+    lambda: Lambda;
+}
+/** @hidden */
+export declare const builtin: BuiltinFrameworks;
+/** @public */
+export interface StandardResponse {
+    /** @public */
+    status: number;
+    /** @public */
+    body: JsonObject;
+    /** @public */
+    headers?: Headers;
+}
+/** @public */
+export interface Headers {
+    /** @public */
+    [header: string]: string | string[] | undefined;
+}
+/** @public */
+export interface StandardHandler {
+    /** @public */
+    (body: JsonObject, headers: Headers): Promise<StandardResponse>;
+}
